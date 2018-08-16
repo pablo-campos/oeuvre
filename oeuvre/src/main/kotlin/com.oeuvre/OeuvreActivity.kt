9dec8b874c06d382067d1e88fcfc039e7497af18
Pablo Campos <pablo.campos@malauzai.com>
2018-08-16 11:14:51 -0500

    Code cleanup.

--- oeuvre/src/main/kotlin/com.oeuvre/OeuvreActivity.kt
+++ oeuvre/src/main/kotlin/com.oeuvre/OeuvreActivity.kt
@@ -5,8 +5,8 @@ import android.support.v7.app.AppCompatActivity
 
 class OeuvreActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_oeuvre)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_oeuvre)
+	}
 }
